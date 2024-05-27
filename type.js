2845f0fcc305e043062279fd87c5142c0176c003
Vincent Lu <luviny2022@gmail.com>
2024-05-27 17:48:57 -0500

    Added proper credits for the icons and fonts

--- type.js
+++ type.js
@@ -1,91 +1,88 @@
 //Get all the elements needed for PokeType
 var types = document.querySelectorAll(".type");
-var url = "https://pokeapi.co/api/v2/type/"
+var url = "https://pokeapi.co/api/v2/type/";
 var effective = document.querySelector("#effective");
 var not_effective = document.querySelector("#not_effective");
 
-
-//
-
-
 //Add the getType function for each type class
-types.forEach(type => type.addEventListener("click", function() {
+types.forEach((type) =>
+  type.addEventListener("click", function () {
     //console.log(type.childNodes[3].innerHTML);
     getType(type.childNodes[3]);
     type.classList.add("selected");
-}));
-
+  }),
+);
 
 /*
-* Function that appends tye type name to the url to get the specified Pokemon type JSON and it's respective weakness, strength, etc
-*/
+ * Function that appends tye type name to the url to get the specified Pokemon type JSON and it's respective weakness, strength, etc
+ */
 async function getType(type) {
-    //First clear the previous Type weakness
-    clearTypes();
+  //First clear the previous Type weakness
+  clearTypes();
 
-    //Get the Pokemon type and append it to the PokeAPI URL so we can process and fetch the data from the PokeAPI JSON
-    var typeUrl = url + type.innerHTML.toLowerCase();
-    //console.log(typeUrl);
-    const response = await fetch(typeUrl);
-    const typeData = await response.json();
+  //Get the Pokemon type and append it to the PokeAPI URL so we can process and fetch the data from the PokeAPI JSON
+  var typeUrl = url + type.innerHTML.toLowerCase();
+  //console.log(typeUrl);
+  const response = await fetch(typeUrl);
+  const typeData = await response.json();
 
-    //Gets the list of weaknesses from the JSON
-    const strengths = typeData.damage_relations.double_damage_to;
-    const weaknesses = typeData.damage_relations.half_damage_to;
+  //Gets the list of weaknesses from the JSON
+  const strengths = typeData.damage_relations.double_damage_to;
+  const weaknesses = typeData.damage_relations.half_damage_to;
 
-    //For each Pokemon type's weakness, add it to the Weakness Table
-    weaknesses.forEach(weakness => {
-        let weakRow = document.createElement("tr");
-        let weakHeader = document.createElement("th");
+  //For each Pokemon type's weakness, add it to the Weakness Table
+  weaknesses.forEach((weakness) => {
+    let weakRow = document.createElement("tr");
+    let weakHeader = document.createElement("th");
 
+    let type = document.getElementById(weakness.name).children[1];
+    let typeCSS = window.getComputedStyle(type);
+    let color = typeCSS.getPropertyValue("background-color");
 
-        let type = document.getElementById(weakness.name).children[1];
-        let typeCSS = window.getComputedStyle(type);
-        let color = typeCSS.getPropertyValue("background-color");
-        
+    weakRow.appendChild(weakHeader);
+    weakHeader.style.backgroundColor = color;
 
-       
-        weakRow.appendChild(weakHeader);
-        weakHeader.style.backgroundColor = color;
+    weakHeader.innerText = weakness.name;
+    weakHeader.innerText =
+      weakHeader.innerText[0].toUpperCase() + weakHeader.innerText.slice(1);
 
-        weakHeader.innerText = weakness.name;
-        weakHeader.innerText = weakHeader.innerText[0].toUpperCase() + weakHeader.innerText.slice(1);;
+    not_effective.appendChild(weakRow);
+  });
 
-        not_effective.appendChild(weakRow);
-    })
+  //For each Pokemon type's strengths, add it to the Strength Table
+  strengths.forEach((strength) => {
+    let strengthRow = document.createElement("tr");
+    let strengthHeader = document.createElement("th");
 
-     //For each Pokemon type's strengths, add it to the Strength Table
-     strengths.forEach(strength => {
-        let strengthRow = document.createElement("tr");
-        let strengthHeader = document.createElement("th");
+    let type = document.getElementById(strength.name).children[1];
+    let typeCSS = window.getComputedStyle(type);
+    let color = typeCSS.getPropertyValue("background-color");
 
-        let type = document.getElementById(strength.name).children[1];
-        let typeCSS = window.getComputedStyle(type);
-        let color = typeCSS.getPropertyValue("background-color");
-       
-        strengthRow.appendChild(strengthHeader);
-        strengthHeader.style.backgroundColor = color;
+    strengthRow.appendChild(strengthHeader);
+    strengthHeader.style.backgroundColor = color;
 
-        strengthHeader.innerText = strength.name;
-        strengthHeader.innerText = strengthHeader.innerText[0].toUpperCase() + strengthHeader.innerText.slice(1);;
+    strengthHeader.innerText = strength.name;
+    strengthHeader.innerText =
+      strengthHeader.innerText[0].toUpperCase() +
+      strengthHeader.innerText.slice(1);
 
-        effective.appendChild(strengthRow);
-    })
-
+    effective.appendChild(strengthRow);
+  });
 }
 
 //Resets the table headers and rows
 function clearTypes() {
-    prev = document.getElementsByClassName("selected");
-    if (prev[0] != undefined) {
-        prev[0].classList.remove("selected");
-    }
+  var prev = document.getElementsByClassName("selected");
 
-    while(not_effective.childNodes.length > 2) {
-        not_effective.removeChild(not_effective.lastChild);
-    }
+  if (prev[0] != undefined) {
+    prev[0].classList.remove("selected");
+  }
 
-    while(effective.childNodes.length > 2) {
-        effective.removeChild(effective.lastChild);
-    }
+  while (not_effective.childNodes.length > 2) {
+    not_effective.removeChild(not_effective.lastChild);
+  }
+
+  while (effective.childNodes.length > 2) {
+    effective.removeChild(effective.lastChild);
+  }
 }
